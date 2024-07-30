58ad9edc508ff7ffe61a75bddef3e7cd0f3145ad
ChoYoungwon <74918493+Youngwonzone1@users.noreply.github.com>
2024-07-31 02:10:50 +0900

    Photo 어플 CRUD 구현 완료

--- photo/urls.py
+++ photo/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.AlbumLV.as_view(), name='index'),
 
     # /photo/album
-    path('album', views.AlbumLV.as_view(), name='album_list'),
+    path('album/', views.AlbumLV.as_view(), name='album_list'),
 
     # /photo/album/99/
     path('album/<int:pk>/', views.AlbumDV.as_view(), name='album_detail'),
@@ -23,10 +23,10 @@ urlpatterns = [
     path('album/change/', views.AlbumChangeLV.as_view(), name='album_change'),
 
     # /photo/album/1/update/
-    path('album/<int:pk>/update', views.AlbumPhotoUV.as_view(), name='album_update'),
+    path('album/<int:pk>/update/', views.AlbumPhotoUV.as_view(), name='album_update'),
 
     # /photo/album/1/delete
-    path('album/<int:pk>/delete', views.AlbumDelV.as_view(), name='album_delete'),
+    path('album/<int:pk>/delete/', views.AlbumDelV.as_view(), name='album_delete'),
 
     # /photo/photo/add/
     path('photo/add/', views.PhotoCV.as_view(), name='photo_add'),
@@ -35,10 +35,10 @@ urlpatterns = [
     path('photo/change/', views.PhotoChangeLV.as_view(), name='photo_change'),
 
     # /photo/photo/1/update/
-    path('photo/<int:pk>/update', views.PhotoUV.as_view(), name='photo_update'),
+    path('photo/<int:pk>/update/', views.PhotoUV.as_view(), name='photo_update'),
 
     # /photo/photo/1/delete/
-    path('photo/<int:pk>/delete', views.PhotoDelV.as_view(), name='photo_delete'),
+    path('photo/<int:pk>/delete/', views.PhotoDelV.as_view(), name='photo_delete'),
 
 
 ]
